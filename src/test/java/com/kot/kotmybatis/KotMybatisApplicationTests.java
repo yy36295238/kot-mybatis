856e0aebe58216ad6c54858736fe6f647d8d98ef
YangYu <36295238@qq.com>
2019-05-24 10:40:53 +0800

    [feat]测试orderBy

--- src/test/java/com/kot/kotmybatis/KotMybatisApplicationTests.java
+++ src/test/java/com/kot/kotmybatis/KotMybatisApplicationTests.java
@@ -39,7 +39,7 @@ public class KotMybatisApplicationTests {
 
     @Test
     public void findOneNoWhere() {
-        final User user = userService.newQuery().findOne(new User());
+        final User user = userService.newQuery().orderBy("id desc").findOne(new User());
         System.err.println(user);
     }
 
@@ -47,8 +47,8 @@ public class KotMybatisApplicationTests {
     public void findOne() {
         final User user = userService.newQuery()
                 .fields(Arrays.asList("id", "user_name", "password"))
-                .eq("id", 15)
-                .isNull("create_user")
+                .eq("id", 37090)
+                .orderBy("id desc")
                 .findOne(User.builder().userStatus(1).build());
         System.err.println(user);
     }
@@ -57,9 +57,6 @@ public class KotMybatisApplicationTests {
     public void list() {
         final List<User> list = userService.newQuery()
                 .fields(Arrays.asList("user_name", "password"))
-                .eq("user_name", "zhangsan")
-                .eq("id", 2)
-                .eq("password", "123")
                 .list(User.builder().userStatus(1).build());
         System.err.println(list);
     }
@@ -68,26 +65,26 @@ public class KotMybatisApplicationTests {
     public void page() {
         final Page<User> page = userService.newQuery()
                 .fields(Arrays.asList("id", "user_name", "password"))
-                .between("id", 1, 12)
-                .selectPage(new Page<>(2, 10, "id", CT.DESC), User.builder().userStatus(1).build());
+                .orderBy("id desc")
+                .selectPage(new Page<>(2, 10), User.builder().userStatus(1).build());
         System.err.println(page);
     }
 
     @Test
     public void delete() {
-        final int delete = userService.newUpdate().eq("user_status", 1).gte("id", 17).delete(User.builder().userName("zhangsan").build());
+        final int delete = userService.newUpdate().eq("user_status", 1).gte("id", 37088).delete(User.builder().userName("wukong0").build());
         System.err.println(delete);
     }
 
     @Test
     public void updateById() {
-        final int update = userService.newUpdate().updateById(User.builder().id(15L).phone("13800138000").build());
+        final int update = userService.newUpdate().updateById(User.builder().id(37092L).phone("13800138000").build());
         System.err.println(update);
     }
 
     @Test
     public void update() {
-        final int update = userService.newUpdate().between("id", 13, 15).update(User.builder().password("123").build(), User.builder().userStatus(1).build());
+        final int update = userService.newUpdate().between("id", 37091, 37095).update(User.builder().password("123").build(), User.builder().userStatus(1).build());
         System.err.println(update);
     }
 
