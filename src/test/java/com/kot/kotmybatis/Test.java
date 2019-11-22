06f73dfed071ea383a95659ef037db8a6562688b
YangYu <36295238@qq.com>
2019-11-22 19:02:28 +0800

    [feat]v2.0.9

--- src/test/java/com/kot/kotmybatis/Test.java
+++ src/test/java/com/kot/kotmybatis/Test.java
@@ -6,7 +6,6 @@ package com.kot.kotmybatis;
  */
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(1000000);
         preMethodName();
     }
 
